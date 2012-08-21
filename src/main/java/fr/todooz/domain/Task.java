7d8688bb1ced8469e83f9f69741a7b70e7ffb87a
damien bourdette <dbourdette@gmail.com>
2012-08-21 23:25:09 +0200

    first commit

---
+++ src/main/java/fr/todooz/domain/Task.java
@@ -0,0 +1,45 @@
+package fr.todooz.domain;
+
+import java.util.Date;
+
+public class Task {
+    private Date createdAt;
+
+    private String text;
+
+    private Date date;
+
+    private String tags;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
