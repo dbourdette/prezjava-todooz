0f36f71218f6ffe7bb7b2e98162ffc7089eea161
damien bourdette <dbourdette@gmail.com>
2012-09-02 17:02:56 +0200

    working on spring

--- src/test/java/fr/todooz/service/TaskServiceTest.java
+++ src/test/java/fr/todooz/service/TaskServiceTest.java
@@ -10,9 +10,14 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fr.todooz.domain.Task;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class TaskServiceTest {
 
 	private SessionFactory sessionFactory;
