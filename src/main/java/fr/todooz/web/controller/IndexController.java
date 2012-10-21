ce139b4ed108ac2171f3a64225e045630b68387c
damien bourdette <dbourdette@gmail.com>
2012-10-21 15:29:15 +0200

    done spring mvc mappings

--- src/main/java/fr/todooz/web/controller/IndexController.java
+++ src/main/java/fr/todooz/web/controller/IndexController.java
@@ -1,14 +1,14 @@
 package fr.todooz.web.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import org.joda.time.DateMidnight;
-import org.joda.time.Interval;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +16,7 @@ import fr.todooz.domain.Task;
 import fr.todooz.service.TagCloudService;
 import fr.todooz.service.TaskService;
 import fr.todooz.util.IntervalUtils;
+import fr.todooz.util.TagCloud;
 
 @Controller
 public class IndexController {
@@ -25,42 +26,38 @@ public class IndexController {
 	@Inject
 	private TagCloudService tagCloudService;
 	
+	@ModelAttribute
+	public TagCloud tagCloud() {
+		return tagCloudService.buildTagCloud();
+	}
+	
 	@RequestMapping({ "/", "/index" })
 	public String index(Model model) {
-		model.addAttribute("tasks", taskService.findAll());
-		model.addAttribute("tagCloud", tagCloudService.buildTagCloud());
-
-		return "index";
+		return page(model, taskService.findAll());
 	}
 	
 	@RequestMapping("/search")
 	public String search(String query, Model model) {
-		model.addAttribute("tasks", taskService.findByQuery(query));
-		model.addAttribute("tagCloud", tagCloudService.buildTagCloud());
-
-		return "index";
+		return page(model, taskService.findByQuery(query));
 	}
 	
 	@RequestMapping("/tag/{tag}")
 	public String tag(@PathVariable String tag, Model model) {
-		model.addAttribute("tasks", taskService.findByTag(tag));
-		model.addAttribute("tagCloud", tagCloudService.buildTagCloud());
-
-		return "index";
+		return page(model, taskService.findByTag(tag));
 	}
 	
 	@RequestMapping("/today")
 	public String today(Model model) {
-		model.addAttribute("tasks", taskService.findByInterval(IntervalUtils.todayInterval()));
-		model.addAttribute("tagCloud", tagCloudService.buildTagCloud());
-
-		return "index";
+		return page(model, taskService.findByInterval(IntervalUtils.todayInterval()));
 	}
 	
 	@RequestMapping("/tomorrow")
 	public String tomorrow(Model model) {
-		model.addAttribute("tasks", taskService.findByInterval(IntervalUtils.tomorrowInterval()));
-		model.addAttribute("tagCloud", tagCloudService.buildTagCloud());
+		return page(model, taskService.findByInterval(IntervalUtils.tomorrowInterval()));
+	}
+	
+	public String page(Model model, List<Task> tasks) {
+		model.addAttribute("tasks", tasks);
 
 		return "index";
 	}
